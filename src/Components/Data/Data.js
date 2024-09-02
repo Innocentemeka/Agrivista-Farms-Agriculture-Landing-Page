d4cfc7536c6bd893d600fa94d1e476118bd74591
Innocentemeka <innocent7932@gmail.com>
2024-09-02 02:07:33 +0100

    added new features

---
+++ src/Components/Data/Data.js
@@ -0,0 +1,44 @@
+const People = [
+  {
+    id: 1,
+    image: "Image/person1.png",
+    name: "Innocent Emeka",
+    title: "Frontend Engineer",
+    quote:
+      "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.",
+  },
+  {
+    id: 2,
+    image: "Image/person2.png",
+    name: "Madeline Williamson",
+    title: "Forward Creative Manager",
+    quote:
+      "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.",
+  },
+  {
+    id: 3,
+    image: "Image/person3.png",
+    name: "Naomi Smith",
+    title: "Product Manager",
+    quote:
+      "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.",
+  },
+  {
+    id: 4,
+    image: "Image/person4.png",
+    name: "Joy Mercy",
+    title: "Backend Engineer",
+    quote:
+      "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.",
+  },
+  {
+    id: 5,
+    image: "Image/person5.png",
+    name: "Michael Sunday",
+    title: "Product Designer",
+    quote:
+      "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.",
+  },
+];
+
+export default People;
--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -1,8 +1,34 @@
 import "./Testimonials.css";
-import photo from "../Images/photo.png";
 import { FaStar } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import People from "../Data/Data";
+import data from "../Data/Data";
+import { FaArrowAltCircleLeft } from "react-icons/fa";
+import { FaArrowAltCircleRight } from "react-icons/fa";
 
 const Testimonials = ({ noOfStars = 5 }) => {
+  const [people] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
   return (
     <div className="testimonial">
       <div className="container">
@@ -17,23 +43,46 @@ const Testimonials = ({ noOfStars = 5 }) => {
           es un myth. Por scientie, musica, sport etc, litot Europa usa li sam
           vocabular.
         </p>
-        <div className="card">
-          <img src={photo} alt="photo" />
-          <div className="card-right">
-            <div className="star-rating">
-              {[...Array(noOfStars)].map((_, index) => {
-                index += 1;
-                return <FaStar key={index} size={25} className="star" />;
-              })}
-            </div>
-            <h3>Madeline Williamson</h3>
-            <span>Forward Creative Manager</span>
-            <h2>
-              Li Europan lingues es membres del sam familie. Lor separat
-              existentie es un myth. Por scientie, musica, sport etc, litot
-              Europa usa li sam vocabular.
-            </h2>
-          </div>
+        <div className="section-center">
+          {People.map((item, indexPeople) => {
+            const { id, name, title, quote } = item;
+            let position = "nextSlide";
+            if (indexPeople === index) {
+              position = "activeSlide";
+            }
+            if (
+              indexPeople === index - 1 ||
+              (index === 0 && indexPeople === people.length - 1)
+            ) {
+              position = "lastSlide";
+            }
+            return (
+              <article className={position} key={id}>
+                <img src={item.image} alt="" className="person-img" />
+                <div className="card-right">
+                  <div className="star-rating">
+                    {[...Array(noOfStars)].map((_, index) => {
+                      index += 1;
+                      return <FaStar key={index} size={25} className="star" />;
+                    })}
+                  </div>
+                  <h3>{name}</h3>
+                  <span>{title}</span>
+                  <h2>{quote}</h2>
+                </div>
+              </article>
+            );
+          })}
+
+          <FaArrowAltCircleLeft
+            className="prev"
+            onClick={() => setIndex(index - 1)}
+          />
+
+          <FaArrowAltCircleRight
+            className="next"
+            onClick={() => setIndex(index + 1)}
+          />
         </div>
       </div>
     </div>
